b8a1dddad9047b2db7d161c449c77faebe2a4089
Chris Diefenthaler <diefentc@lcc.mail.edu>
2025-02-20 22:07:57 -0500

    implemented example files from zip archive

---
+++ html/exercises/exercise05/js/letter_grade.js
@@ -0,0 +1,11 @@
+
+function calc(){
+        var entry = parseInt(document.getElementById("score").value);
+        var letterGrade;
+        //write the if statements in this area below, leaving the last curly brace at the end of the script.
+
+
+
+
+
+}//don't remove this line
